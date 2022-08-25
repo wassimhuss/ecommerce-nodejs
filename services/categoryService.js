9e2f5824d9410975b5b4c839adf85395493e6104
wassim hussein <72971087+wassimhuss@users.noreply.github.com>
2022-08-25 15:49:02 +0000

    delete img when delete brand or category

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,27 +1,28 @@
-const sharp = require('sharp');
-const { v4: uuidv4 } = require('uuid');
-const asyncHandler = require('express-async-handler');
+const sharp = require("sharp");
+const { v4: uuidv4 } = require("uuid");
+const asyncHandler = require("express-async-handler");
 
-const factory = require('./handlersFactory');
-const { uploadSingleImage } = require('../middlewares/uploadImageMiddleware');
-const Category = require('../models/categoryModel');
+const factory = require("./handlersFactory");
+const { uploadSingleImage } = require("../middlewares/uploadImageMiddleware");
+const Category = require("../models/categoryModel");
 
 // Upload single image
-exports.uploadCategoryImage = uploadSingleImage('image');
+exports.uploadCategoryImage = uploadSingleImage("image");
 
 // Image processing
 exports.resizeImage = asyncHandler(async (req, res, next) => {
-  const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
+  if (req.file) {
+    const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
 
-  await sharp(req.file.buffer)
-    .resize(600, 600)
-    .toFormat('jpeg')
-    .jpeg({ quality: 95 })
-    .toFile(`uploads/categories/${filename}`);
+    await sharp(req.file.buffer)
+      .resize(600, 600)
+      .toFormat("jpeg")
+      .jpeg({ quality: 95 })
+      .toFile(`uploads/categories/${filename}`);
 
-  // Save image into our db
-  req.body.image = filename;
-
+    // Save image into our db
+    req.body.image = filename;
+  }
   next();
 });
 
--- services/handlersFactory.js
+++ services/handlersFactory.js
@@ -1,24 +1,43 @@
-const asyncHandler = require('express-async-handler');
-const ApiError = require('../utils/apiError');
-const ApiFeatures = require('../utils/apiFeatures');
-
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/apiError");
+const ApiFeatures = require("../utils/apiFeatures");
+const fs = require("fs");
 exports.deleteOne = (Model) =>
   asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-    const document = await Model.findByIdAndDelete(id);
-
+    const oldProduct = await Model.findOne({ _id: req.params.id });
+    const oldImage = oldProduct.image.split("http://localhost:8000/")[1];
+    fs.unlink("C://projects/ecommerce-apis/uploads/" + oldImage, (err) => {
+      if (err) {
+        // file doens't exist
+        console.info("File doesn't exist, won't remove it.");
+      } else {
+        console.info(`removed`);
+      }
+    });
+    const document = await Model.findByIdAndDelete(req.params.id);
     if (!document) {
-      return next(new ApiError(`No document for this id ${id}`, 404));
+      return next(
+        new ApiError(`No document for this id ${req.params.id}`, 404)
+      );
     }
     res.status(204).send();
   });
 
 exports.updateOne = (Model) =>
   asyncHandler(async (req, res, next) => {
+    const oldProduct = await Model.findOne({ _id: req.params.id });
+    const oldImage = oldProduct.image.split("http://localhost:8000/")[1];
+    fs.unlink("C://projects/ecommerce-apis/uploads/" + oldImage, (err) => {
+      if (err) {
+        // file doens't exist
+        console.info("File doesn't exist, won't remove it.");
+      } else {
+        console.info(`removed`);
+      }
+    });
     const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
-
     if (!document) {
       return next(
         new ApiError(`No document for this id ${req.params.id}`, 404)
@@ -43,7 +62,7 @@ exports.getOne = (Model) =>
     res.status(200).json({ data: document });
   });
 
-exports.getAll = (Model, modelName = '') =>
+exports.getAll = (Model, modelName = "") =>
   asyncHandler(async (req, res) => {
     let filter = {};
     if (req.filterObj) {
